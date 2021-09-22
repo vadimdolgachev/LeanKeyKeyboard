5da94ccb19c3306f050639cdc8f4cdb6e8258d3a
vadim <vadimdolgachev@gmail.com>
2021-09-22 15:26:04 +0700

    change color scheme

--- leankeykeyboard/src/main/java/com/liskovsoft/leankeyboard/activity/settings/KbSettingsActivity2.java
+++ leankeykeyboard/src/main/java/com/liskovsoft/leankeyboard/activity/settings/KbSettingsActivity2.java
@@ -2,10 +2,14 @@ package com.liskovsoft.leankeyboard.activity.settings;
 
 import android.os.Bundle;
 
+import com.liskovsoft.leankeyboard.addons.keyboards.extkeyboards.utils.log.BuildConfig;
+
 public class KbSettingsActivity2 extends KbSettingsActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        finish();
+        if (BuildConfig.DEBUG) {
+            finish();
+        }
     }
 }
